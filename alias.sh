e5d15474dc9995f801686f9e02ad5e28e1bf86b9
ccyanxyz <790266922@qq.com>
2020-10-15 18:33:41 +0800

    init

---
+++ alias.sh
@@ -0,0 +1,2 @@
+echo 'alias cl="clear"' >> ~/.ssh/bash_profile
+echo 'alias py="python3"' >> ~/.ssh/bash_profile
---
+++ py3.7_centos.sh
@@ -0,0 +1,3 @@
+yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gcc make libffi-devel
+pyenv install 3.7.0
+pyenv global 3.7.0
